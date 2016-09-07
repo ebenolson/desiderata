fb14c254f381837ef336fa66e4bc0ec4ba69a52e
Eben Olson <eben.olson@gmail.com>
2016-09-07 13:15:08 -0400

    cleanup, py3 compatibility, add some more info logging

--- desiderata.py
+++ desiderata.py
@@ -7,20 +7,21 @@ import hashlib
 import logging
 from colorlog import ColoredFormatter
 import plac
-import sys, os
+import sys
+import os
 from fs import zipfs
 
 formatter = ColoredFormatter(
-        "%(log_color)s%(levelname)-8s%(reset)s %(white)s%(message)s",
-        datefmt=None,
-        reset=True,
-        log_colors={
-                'DEBUG':    'cyan',
-                'INFO':     'green',
-                'WARNING':  'yellow',
-                'ERROR':    'red',
-                'CRITICAL': 'red',
-        }
+    "%(log_color)s%(levelname)-8s%(reset)s %(white)s%(message)s",
+    datefmt=None,
+    reset=True,
+    log_colors={
+        'DEBUG':    'cyan',
+        'INFO':     'green',
+        'WARNING':  'yellow',
+        'ERROR':    'red',
+        'CRITICAL': 'red',
+    }
 )
 
 stream = logging.StreamHandler()
@@ -29,6 +30,7 @@ stream.setFormatter(formatter)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(stream)
+
 
 def read_blocks(filename):
     with open(filename, 'rb') as f:
@@ -40,6 +42,7 @@ def read_blocks(filename):
 
 algos = ('md5', 'sha1', 'sha512')
 
+
 class Hasher(object):
     '''Calculate multiple hash digests for a piece of data.'''
     def __init__(self, algos):
@@ -47,14 +50,17 @@ class Hasher(object):
         self._hashes = {}
         for algo in self.algos:
             self._hashes[algo] = getattr(hashlib, algo)()
+
     def update(self, data):
         for h in self._hashes.values():
             h.update(data)
+
     def hexdigests(self):
         '''Yields the algorithm and the calculated hex digest.'''
         for algo in self.algos:
             digest = self._hashes[algo].hexdigest()
             yield algo, digest
+
 
 def calculate_hash(filename):
     hasher = Hasher(algos)
@@ -64,63 +70,82 @@ def calculate_hash(filename):
 
     return u''.join((u'{0} {1}\n'.format(algo, digest) for algo, digest in hasher.hexdigests()))
 
+
 def check_structure(rootdir, hashfile):
     logger.info('Checking directory structure')
 
+    count = 0
     encoding = sys.getfilesystemencoding()
     with zipfs.ZipFS(hashfile, 'r', encoding=encoding) as hashfs:
         for root, dirs, files in os.walk(rootdir):
             for name in files:
+                count += 1
                 filename = unicode(os.path.join(root,name), encoding=encoding)
+                logfilename = ''.join(filename.split(rootdir)[1:])
                 logger.debug(filename)
-                if not hashfs.isfile(filename):
+                if not hashfs.isfile(logfilename):
                     logger.warn(u'Hash missing: {0}'.format(filename))
 
-        for filename in hashfs.walkfiles(rootdir):
+        for logfilename in hashfs.walkfiles('/'):
+            filename = os.path.join(rootdir, logfilename[1:])
             logger.debug(filename)
             if not os.path.isfile(filename):
                 logger.error(u'File not found: {0}'.format(filename))
-    logger.info(u'Check complete')   
+    logger.info(u'Check complete, located {} files'.format(count))
+
 
 def verify_hashes(rootdir, hashfile):
     logger.info('Verifying all hashes')
 
+    count = 0
+
     encoding = sys.getfilesystemencoding()
     with zipfs.ZipFS(hashfile, 'r', encoding=encoding) as hashfs:
-        for filename in hashfs.walkfiles(rootdir):
+        for logfilename in hashfs.walkfiles('/'):
+            count += 1
+            filename = os.path.join(rootdir, logfilename[1:])
             logger.debug(filename)
             if not os.path.isfile(filename):
                 logger.error(u'File not found: {0}'.format(filename))
-            elif hashfs.open(filename).read() != calculate_hash(filename):
+            elif hashfs.open(logfilename).read() != calculate_hash(filename):
                 logger.error(u'Hash verification error: {0}'.format(filename))
-    logger.info(u'Verification complete')   
+    logger.info(u'Verification complete, {} files checked'.format(count))
+
 
 def record_hashes(rootdir, hashfile):
-    logger.info('Recording all hashes')   
+    logger.info('Recording all hashes')
     if os.path.exists(hashfile):
         logger.error(u'Output file already exists!')
         return
 
+    count = 0
     encoding = sys.getfilesystemencoding()
     with zipfs.ZipFS(hashfile, 'w', encoding=encoding) as hashfs:
         hashfs.makedir(rootdir)
         for root, dirs, files in os.walk(rootdir):
             for dir in dirs:
+                if os.path.islink(os.path.join(root, dir)):
+                    continue
                 hashfs.makedir(os.path.join(root, dir))
             for name in files:
-                filename = unicode(os.path.join(root,name), encoding=encoding)
-                logger.debug(filename)
-                with hashfs.open(filename,'w') as hashfile:
+                count += 1
+                filename = unicode(os.path.join(root, name), encoding=encoding)
+                if os.path.islink(filename):
+                    continue
+                logfilename = ''.join(filename.split(rootdir)[1:])
+                logger.debug('{}: {}'.format(filename, logfilename))
+                with hashfs.open(logfilename, 'w') as hashfile:
                     hashfile.write(calculate_hash(filename))
         hashfs.close()
-    logger.info(u'Recording complete')   
+    logger.info(u'Recording complete, hashed {} files'.format(count))
+
 
 @plac.annotations(
-    record=('Record hashes for all files', 'flag', 'r'), 
-    check=('Check directory for missing or added files', 'flag', 'c'), 
+    record=('Record hashes for all files', 'flag', 'r'),
+    check=('Check directory for missing or added files', 'flag', 'c'),
     verify=('Verify hashes for all files', 'flag', 'v'),
-    debug=('Show all log output', 'flag', 'd'), 
-    target='Target directory',    
+    debug=('Show all log output', 'flag', 'd'),
+    target='Target directory',
     )
 def main(record, check, verify, debug, target, outfile='hashes.zip'):
     if debug:
